bae4278464abdd364c8547d5a41459e7cb14caf3
Zennal <zennal.int@gmail.com>
2024-08-29 11:35:57 +0100

    fix 419 issues

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,7 +16,7 @@
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    {{-- <meta name="csrf-token" content="{{ csrf_token() }}"> --}}
     <title> {{env('APP_NAME')}} - Enjoy data at the best rate </title>
     <meta name="description" content="Empowering Connections, One Byte at a Time - {{ env('APP_NAME') }}">
     <meta name="keywords" content="data purchase, mtn, airtel, utility bills, cable subscription">
