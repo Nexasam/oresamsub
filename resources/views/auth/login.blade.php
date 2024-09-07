2331a613b2c8ba4a24a69c8fe105a90f022b29b3
Zennal <zennal.int@gmail.com>
2024-09-07 04:25:06 +0100

    fix loggin

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -115,7 +115,7 @@
                             <br>
 
                             <div class="text-center mb-4">
-                                <h1 class="block text-2xl font-bold text-gray-800 dark:text-white">Sign in</h1>
+                                <h1 class="block text-2xl font-bold text-gray-800 dark:text-white">Sign in.</h1>
                                 <p class="mt-3 text-sm text-gray-600 dark:text-white/70">
                                     Don't have an account yet?
                                     <a class="text-primary decoration-2 hover:underline font-medium"  href="{{route('register')}}">
