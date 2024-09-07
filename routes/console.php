333ca3327ee56c0a9b41b4ae3d399009f747c840
Zennal <zennal.int@gmail.com>
2024-09-07 03:03:54 +0100

    fix loggin

--- routes/console.php
+++ routes/console.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
-    Schedule::command('php artisan migrate')->everyMinute();
-    logger('running...');
+    // Schedule::command('php artisan migrate')->everyMinute();
+    
 })->purpose('Display an inspiring quote')->hourly();
-// Schedule::command('php artisan migrate')->everyFiveMinutes();
+Schedule::command('php artisan migrate')->everyFiveMinutes();
