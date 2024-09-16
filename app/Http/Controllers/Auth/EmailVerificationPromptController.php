a0b8fd3041480df0c2cb5abecd07d10191839d0c
Zennal <zennal.int@gmail.com>
2024-09-16 14:57:46 +0100

    fix issues on verification

--- app/Http/Controllers/Auth/EmailVerificationPromptController.php
+++ app/Http/Controllers/Auth/EmailVerificationPromptController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\View\View;
+use Illuminate\Http\Request;
+use App\Models\LandingPagesSetting;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
-use Illuminate\View\View;
 
 class EmailVerificationPromptController extends Controller
 {
@@ -14,8 +15,12 @@ class EmailVerificationPromptController extends Controller
      */
     public function __invoke(Request $request): RedirectResponse|View
     {
+        $landing_data = LandingPagesSetting::get();
+        foreach($landing_data as $landing_component){
+            $data[$landing_component->field_name] = $landing_component->field_details;
+        }
         return $request->user()->hasVerifiedEmail()
                     ? redirect()->intended(route('dashboard', absolute: false))
-                    : view('auth.verify-email');
+                    : view('auth.verify-email')->with($data);
     }
 }
