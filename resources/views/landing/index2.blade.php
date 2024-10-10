228b9125d65184425f90b5d88581d8c4756c14be
Zennal <zennal.int@gmail.com>
2024-10-10 08:26:21 +0100

    get the footer set

--- resources/views/landing/index2.blade.php
+++ resources/views/landing/index2.blade.php
@@ -277,7 +277,7 @@
             <div class="sm:flex sm:items-center sm:justify-between">
                 {{-- <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com" class="hover:underline">Flowbite™</a>. All Rights Reserved.
                 </span> --}}
-                <p class="mb-0" style="color: white">Developed with ❤️ by Subutility 	&#169; {{ date('Y') }} . All rights Reserved</p>
+                <p class="mb-0" style="color: white">Developed with ❤️ by <a href="https://api.whatsapp.com/send?phone={{  '2347073459839'  }}&text=Hello,%20Please%20I%20want%20to%20own%20a%20data%20website" style="color: white;">Subutility</a> 	&#169; {{ date('Y') }} . All rights Reserved</p>
                 <div class="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
                     <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
                         <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd" /></svg>
