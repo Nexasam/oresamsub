cd5d432df0b786936616a563d8b3d8f82cd5d0a2
Zennal <zennal.int@gmail.com>
2024-10-16 05:40:51 +0100

    notify users of new registration

--- app/Mail/UserRegistrationNotification.php
+++ app/Mail/UserRegistrationNotification.php
@@ -30,7 +30,7 @@ class UserRegistrationNotification extends Mailable
     {
         return new Envelope(
             from: new Address(env('MAIL_FROM_ADDRESS'), env('APP_NAME')),
-            subject: 'A New User Just Registered',
+            subject: 'A New User Just Registered on'. env('APP_NAME'),
         );
     }
 
