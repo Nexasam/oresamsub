bbc65a6af20fb6bf907aabf800c2ef3d7e7659eb
Zennal <zennal.int@gmail.com>
2024-10-16 04:10:29 +0100

    remove percentage from wema

--- app/Http/Controllers/WalletsController.php
+++ app/Http/Controllers/WalletsController.php
@@ -425,8 +425,8 @@ class WalletsController extends Controller
     public function index(Request $request){
         // dd('good');
         $user_id = auth()->id();
-        $funding_option = FundingOption::with('bank_codes.virtual_user_account_with_bank_code')->where('activation_status',1)->first();
-        // $funding_option = FundingOption::with('bank_codes')->where('activation_status',1)->first();
+        // $funding_option = FundingOption::with('bank_codes.virtual_user_account_with_bank_code')->where('activation_status',1)->first();
+        $funding_option = FundingOption::with('bank_codes')->where('activation_status',1)->first();
         $data['funding_option'] = $funding_option;
 
         $generated_user_virtual_accts_funding_option_id = UserVirtualAccount::where('user_id',auth()->id())->pluck('funding_option_id')->first();
