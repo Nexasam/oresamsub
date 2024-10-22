72ed783550c7356843a2ae4cc4e498d3343c3be1
Zennal <zennal.int@gmail.com>
2024-10-22 14:19:19 +0100

    fix issue on ui

--- resources/views/admin/automations/megasubplug_dashboard.blade.php
+++ resources/views/admin/automations/megasubplug_dashboard.blade.php
@@ -21,7 +21,7 @@
         <!-- Start::row-1 -->
         <div class="grid grid-cols-12 gap-1">
 
-          <div class="col-span-12 md:col-span-6 xxl:!col-span-12">
+          <div class="col-span-12 md:col-span-12 xxl:!col-span-12">
             <div class="box">
                 <div class="box-header">
                     <h5 class="box-title">Automation Settings for {{ $automation->automation_name }}</h5>
