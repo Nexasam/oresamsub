7448854f6c516b5718e578abea0f50aaeed32801
Zennal <zennal.int@gmail.com>
2024-10-30 11:18:29 +0100

    adjusted 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -30,7 +30,7 @@
             <div class="text-center py-10 px-4 sm:px-6 lg:px-8">
                 <h1 class="block font-bold text-primary text-9xl dark:text-primary">404</h1>
                 <p class="mt-3 text-2xl font-bold text-gray-800 dark:text-white">Oops, something went wrong.</p>
-                <p class="text-gray-600 dark:text-white/70">Sorry, we couldn't find your page. Use any of the links below to get back on track</p>
+                <p class="text-gray-600 dark:text-white/70">Sorry, we couldn't find your page. Use the link below to get back on track</p>
                 <div class="mt-5 flex flex-col justify-center items-center gap-2 sm:flex-row sm:gap-3">
                     <a class="w-full sm:w-auto inline-flex justify-center items-center gap-x-3 text-center bg-primary hover:bg-primary border border-transparent text-white text-sm font-medium rounded-sm focus:outline-none focus:ring-0 focus:ring-primary focus:ring-offset-0 focus:ring-offset-white transition py-2 px-3 dark:focus:ring-offset-white/10"
                         href="{{route('dashboard')}}">
