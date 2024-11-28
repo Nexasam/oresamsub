51a0b66a1f2db8a060d03b3889093076e01a272a
Zennal <zennal.int@gmail.com>
2024-11-28 11:37:02 +0100

    fix dashboard display

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -245,7 +245,7 @@
                             <div class="flex space-x-4 rtl:space-x-reverse">
                             
                                 <div class="flex items-center justify-center ecommerce-icon px-0">
-                                    <span class="rounded-sm p-4 bg-info/10">
+                                    <span class="rounded-sm p-4 bg-danger/10">
                                         <svg xmlns="http://www.w3.org/2000/svg" class="fill-white svg5"
                                             enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24"
                                             width="24px" fill="#000000">
@@ -327,7 +327,7 @@
                     <div class="flex space-x-4 rtl:space-x-reverse">
                         
                             <div class="flex items-center justify-center ecommerce-icon px-0">
-                                <span class="rounded-sm p-4 bg-info/10">
+                                <span class="rounded-sm p-4 bg-success/10">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="fill-white svg5"
                                         enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24"
                                         width="24px" fill="#000000">
@@ -364,7 +364,7 @@
                     <div class="flex space-x-4 rtl:space-x-reverse">
                         
                             <div class="flex items-center justify-center ecommerce-icon px-0">
-                                <span class="rounded-sm p-4 bg-info/10">
+                                <span class="rounded-sm p-4 bg-danger/10">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="fill-white svg5"
                                         enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24"
                                         width="24px" fill="#000000">
