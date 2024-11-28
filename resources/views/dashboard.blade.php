e9bb6c5b35cd20db9e18aa571ff360f4b99d4567
Zennal <zennal.int@gmail.com>
2024-11-28 11:44:47 +0100

    fix dashboard display

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -246,19 +246,12 @@
                             
                                 <div class="flex items-center justify-center ecommerce-icon px-0">
                                     <span class="rounded-sm p-4 bg-danger/10">
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="fill-white svg5"
-                                            enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24"
-                                            width="24px" fill="#000000">
-                                            <path d="M0,0h24v24H0V0z" fill="none" />
-                                            <g>
-                                                <path
-                                                    d="M19.5,3.5L18,2l-1.5,1.5L15,2l-1.5,1.5L12,2l-1.5,1.5L9,2L7.5,3.5L6,2v14H3v3c0,1.66,1.34,3,3,3h12c1.66,0,3-1.34,3-3V2 L19.5,3.5z M15,20H6c-0.55,0-1-0.45-1-1v-1h10V20z M19,19c0,0.55-0.45,1-1,1s-1-0.45-1-1v-3H8V5h11V19z" />
-                                                <rect height="2" width="6" x="9" y="7" />
-                                                <rect height="2" width="2" x="16" y="7" />
-                                                <rect height="2" width="6" x="9" y="10" />
-                                                <rect height="2" width="2" x="16" y="10" />
-                                            </g>
-                                        </svg>
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="fill-white svg6" height="24px"
+                                        viewBox="0 0 24 24" width="24px" fill="#000000">
+                                        <path d="M0 0h24v24H0V0z" fill="none" />
+                                        <path
+                                            d="M16.5 3c-1.74 0-3.41.81-4.5 2.09C10.91 3.81 9.24 3 7.5 3 4.42 3 2 5.42 2 8.5c0 3.78 3.4 6.86 8.55 11.54L12 21.35l1.45-1.32C18.6 15.36 22 12.28 22 8.5 22 5.42 19.58 3 16.5 3zm-4.4 15.55l-.1.1-.1-.1C7.14 14.24 4 11.39 4 8.5 4 6.5 5.5 5 7.5 5c1.54 0 3.04.99 3.57 2.36h1.87C13.46 5.99 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5 0 2.89-3.14 5.74-7.9 10.05z" />
+                                    </svg>
                                     </span>
                                 </div>
                                 <div class="">
