d30543499feb37eeaa7d57ca3661281d4daecc41
Sammiee4real <adebsholey4real@gmail.com>
2024-12-25 14:33:07 +0100

    trying to reduce server usage

--- routes/console.php
+++ routes/console.php
@@ -8,29 +8,19 @@ use App\Console\Commands\SendNewRegistrationEmail;
 use App\Console\Commands\ZerorizeNegativeBalances;
 use App\Console\Commands\ProcessPendingAirtimeTransactions;
 
-
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-    // Schedule::command('php artisan migrate')->everyMinute();    
-})->purpose('Display an inspiring quote')->hourly();
-
-// Artisan::command('logs:clear', function() {
-    
-//     exec('rm -f ' . storage_path('logs/*.log'));
-
-//     exec('rm -f ' . base_path('*.log'));
-    
-//     $this->comment('Logs have been cleared!');
-    
-// })->describe('Clear log files');
-
-
-// Schedule::command('log:clear --force')->everyMinute();
+// Artisan::command('inspire', function () {
+//     $this->comment(Inspiring::quote());
+//     // Schedule::command('php artisan migrate')->everyMinute();    
+// })->purpose('Display an inspiring quote')->hourly();
 
 // Schedule::command('clear-error-logs')->everyMinute()->withoutOverlapping();
 Schedule::command('migrate --force')->everyMinute();
 Schedule::command(ProcessPendingAirtimeTransactions::class)->everyFifteenSeconds()->withoutOverlapping();
-Schedule::command(ZerorizeNegativeBalances::class)->everyTenSeconds()->withoutOverlapping();
-Schedule::command(SendNewRegistrationEmail::class)->everyFiveMinutes()->withoutOverlapping();
+
+// Schedule::command(ZerorizeNegativeBalances::class)->everyTenSeconds()->withoutOverlapping();
+Schedule::command(ZerorizeNegativeBalances::class)->everyTwoMinutes()->withoutOverlapping();
+
+// Schedule::command(SendNewRegistrationEmail::class)->everyFiveMinutes()->withoutOverlapping();
+Schedule::command(SendNewRegistrationEmail::class)->everyFourMinutes()->withoutOverlapping();
 // Schedule::command(ClearErrorLogs::class)->everyFifteenSeconds()->withoutOverlapping();
 
