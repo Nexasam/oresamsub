8406070c0cc194dd4c2fa7f378de8bd5b0af182a
Zennal <zennal.int@gmail.com>
2025-02-27 22:57:42 +0100

    price change

--- app/Http/Controllers/PriceChangeController.php
+++ app/Http/Controllers/PriceChangeController.php
@@ -9,13 +9,14 @@ class PriceChangeController extends Controller
 {
     //currently for megasubplug
     public function changeMegasubPrice(){
-        $plan_category_id_smemtn = '9c39f216-00a0-42ab-b195-558133f67a15';
+        // $plan_category_id_smemtn = '9c39f216-00a0-42ab-b195-558133f67a15'; //mtn sme
+        $plan_category_id_cgmtn = '9c39f216-06d6-48fc-971e-d5778723497e';
         $mtncostprice = 640;
         $mtnsprice1 = 660;
         $mtnsprice2 = 655;
         $mtnsprice3 = 650;
         $mtnsprice4 = 645;
-        $product_plans = ProductPlan::where('product_plan_category_id',$plan_category_id_smemtn)
+        $product_plans = ProductPlan::where('product_plan_category_id',$plan_category_id_cgmtn)
                          ->where('automation_id','9c2887ea-55b5-4f19-904e-e490a10682ea')
                          ->get();
         foreach($product_plans as $product_plan){
@@ -45,14 +46,14 @@ class PriceChangeController extends Controller
             echo "Selling price4: $old_user_level_4_selling_price : $mtnprice44<br>";
             echo "<hr><hr><hr>";
 
-            ProductPlan::where('id',$product_plan->id)->update([
-                'cost_price' => $newmtncostprice,
-                'default_selling_price' => $mtnprice11,
-                'user_level_1_selling_price' => $mtnprice11,
-                'user_level_2_selling_price' => $mtnprice22,
-                'user_level_3_selling_price' => $mtnprice33,
-                'user_level_4_selling_price' => $mtnprice44,
-            ]);
+            // ProductPlan::where('id',$product_plan->id)->update([
+            //     'cost_price' => $newmtncostprice,
+            //     'default_selling_price' => $mtnprice11,
+            //     'user_level_1_selling_price' => $mtnprice11,
+            //     'user_level_2_selling_price' => $mtnprice22,
+            //     'user_level_3_selling_price' => $mtnprice33,
+            //     'user_level_4_selling_price' => $mtnprice44,
+            // ]);
         }
         
         
