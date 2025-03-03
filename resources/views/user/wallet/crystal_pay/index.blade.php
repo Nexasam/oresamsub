2df70e35bacbe4c108bcd3bbba8f5ee7258c7cff
Zennal <zennal.int@gmail.com>
2025-03-03 07:29:12 +0100

    fix percentage display

--- resources/views/user/wallet/crystal_pay/index.blade.php
+++ resources/views/user/wallet/crystal_pay/index.blade.php
@@ -101,7 +101,7 @@
                                           <tr aria-colspan="3">
                                           
                                             <td>Bank Name: {{ $bank_code->bank_name }} 
-                                              @if (strtolower($bank_code->bank_name) == 'wema bank' || strtotime($bank_code->bank_name) == 'palmpay' )
+                                              @if (strtolower($bank_code->bank_name) == 'wema bank' || strtolower($bank_code->bank_name) == 'palmpay' )
                                                 <br> Charges: 	&#8358;{{ $bank_code->bank_charges }}                                                 
                                               @else
                                                 <br> Charges: {{ $bank_code->bank_charges }} %
