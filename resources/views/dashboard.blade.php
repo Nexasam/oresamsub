034d64c0303bba8ca2021d4a2256c8131a24efc8
Sammiee4real <adebsholey4real@gmail.com>
2025-05-24 13:37:11 +0100

    add vaccounts

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -149,7 +149,7 @@
             </div>
 
             <div
-             class="max-w-sm w-full p-4 mt-2 rounded-2xl shadow-lg bg-gradient-to-r from-{{ ['blue','yellow','red','green'][rand(0,3)] }}-500 to-{{ ['blue','yellow','red','green'][rand(0,3)] }}-700 text-white relative space-y-4"
+             class="max-w-sm w-full p-4 mt-2 rounded-2xl shadow-lg bg-{{ ['blue','yellow','red','green'][rand(0,3)] }}-500  text-white relative space-y-4"
             >
 
                 @foreach ($user_virtual_accounts as $vaccount)
