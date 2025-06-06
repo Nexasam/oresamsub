36fa5583067c0f11d4273c885567164047fc5436
Sammiee4real <adebsholey4real@gmail.com>
2025-06-06 08:59:44 +0100

    allow generation without pin

--- app/Traits/JsonResponseWrapperMobile.php
+++ app/Traits/JsonResponseWrapperMobile.php
@@ -42,32 +42,7 @@ trait JsonResponseWrapperMobile{
 
     public function error($message, $data = [], $code = 500, $access = [])
     {
-        if(count($data) <= 0 && count($access) >= 1){
-            return response()->json([
-                'status' => true,
-                'code' => 200,
-                'message' => $message,
-                'access' => $access,
-            ]);
-        }
-
-        if(count($access) <= 0 && count($data) >= 1){
-            return response()->json([
-                'status' => true,
-                'code' => 200,
-                'message' => $message,
-                'data' => $data,
-            ]);
-        }
-
-        if(count($access) <= 0 && count($data) <= 0){
-            return response()->json([
-                'status' => true,
-                'code' => 200,
-                'message' => $message,
-            ]);
-        }
-
+       
        return  response()->json([
             'status' => false,
             'code' => $code,
