bea1d4ac321ee35ad95d8cf3d8295f2c6ae8b4dd
Sammiee4real <adebsholey4real@gmail.com>
2025-06-06 08:33:57 +0100

    allow generation without pin

--- app/Traits/JsonResponseWrapperMobile.php
+++ app/Traits/JsonResponseWrapperMobile.php
@@ -5,6 +5,32 @@ namespace App\Traits;
 trait JsonResponseWrapperMobile{
   
     public function success($message = 'success', $data = [], $access = []){
+        if(count($data) <= 0 && count($access) >= 1){
+            return response()->json([
+                'status' => true,
+                'code' => 200,
+                'message' => $message,
+                'access' => $access,
+            ]);
+        }
+
+        if(count($access) <= 0 && count($data) >= 1){
+            return response()->json([
+                'status' => true,
+                'code' => 200,
+                'message' => $message,
+                'data' => $data,
+            ]);
+        }
+
+        if(count($access) <= 0 && count($data) <= 0){
+            return response()->json([
+                'status' => true,
+                'code' => 200,
+                'message' => $message,
+            ]);
+        }
+
         return response()->json([
             'status' => true,
             'code' => 200,
@@ -16,6 +42,32 @@ trait JsonResponseWrapperMobile{
 
     public function error($message, $data = [], $code = 500, $access = [])
     {
+        if(count($data) <= 0 && count($access) >= 1){
+            return response()->json([
+                'status' => true,
+                'code' => 200,
+                'message' => $message,
+                'access' => $access,
+            ]);
+        }
+
+        if(count($access) <= 0 && count($data) >= 1){
+            return response()->json([
+                'status' => true,
+                'code' => 200,
+                'message' => $message,
+                'data' => $data,
+            ]);
+        }
+
+        if(count($access) <= 0 && count($data) <= 0){
+            return response()->json([
+                'status' => true,
+                'code' => 200,
+                'message' => $message,
+            ]);
+        }
+        
        return  response()->json([
             'status' => false,
             'code' => $code,
