4e0438c1497d179c51c32b8579f1b61d0650a086
Sammiee4real <adebsholey4real@gmail.com>
2025-06-12 13:56:03 +0100

    allow username

--- app/Http/Controllers/ExternalIntegration/ApiIntegrationController.php
+++ app/Http/Controllers/ExternalIntegration/ApiIntegrationController.php
@@ -265,7 +265,7 @@ class ApiIntegrationController extends Controller
      public function login(Request $request)
      {
         $request->validate([
-             'email' => 'required|email|exists:users,email',
+             'email' => 'required',
              'password' => 'required',
              'device_name' => 'required',
          ]);
