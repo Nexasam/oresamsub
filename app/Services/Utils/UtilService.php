6f603b1626269c7ec2ddd43be71f773e47c0d93c
Sammiee4real <adebsholey4real@gmail.com>
2025-06-23 18:03:25 +0100

    adjust prefix

--- app/Services/Utils/UtilService.php
+++ app/Services/Utils/UtilService.php
@@ -82,13 +82,12 @@ class UtilService{
             $phone_num_no_234 = "0$extracted_number";
         }
 
-       $validated_phone_number = $shouldHave234 ? $phone_num_234 : $phone_num_no_234;
+        $validated_phone_number = $shouldHave234 ? $phone_num_234 : $phone_num_no_234;
 
-       $number_prefix = substr($validated_phone_number, 0, 4);
+        $number_prefix = substr($validated_phone_number, 0, 4);
 
-    
-        $mtnPrefixes = ["0803","0806","0703","0706","0707","0810","0813","0814","0816","0903","0906","0913","0916",'0704'];
-        $airtelPrefixes = ["0802","0808","0701","0708","0812","0902","0907","0901","0912"];
+        $mtnPrefixes = ["0803","0806","0703","0706","0707","0810","0813","0814","0816","0903","0906","0913","0916",'0704','0702'];
+        $airtelPrefixes = ["0802","0808","0701","0708","0812","0902","0907","0901","0912","0911"];
         $gloPrefixes = ["0805","0807","0705","0811","0815","0905","0915"];
         $etisalatPrefixes = ["0809","0817", "0818","0909","0908"]; //9mobile
 
