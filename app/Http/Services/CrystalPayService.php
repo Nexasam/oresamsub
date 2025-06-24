ee7aaeb00997d2037a4c8eb8d871267d860fa2fa
Sammiee4real <adebsholey4real@gmail.com>
2025-06-24 18:37:59 +0100

    adjust prefix

--- app/Http/Services/CrystalPayService.php
+++ app/Http/Services/CrystalPayService.php
@@ -44,7 +44,7 @@ class CrystalPayService{
                 $check_va = UserVirtualAccount::where('user_id',$user_id)->where('bank_code',$bank_code->bank_code)->first();
                 if($check_va){
                         //dont generate
-                        logger('seems generated already for '.$first_name.' bankcode: '.$bank_code);
+                        logger('seems generated already for '.$first_name.' bankcode: '.$bank_code->bank_code);
                  
                 }else{
                     //generate
@@ -104,8 +104,11 @@ class CrystalPayService{
                             'bvn' => $phone_number
                         ]);
                         logger("VA GENERATED INDEED FOR $first_name | $user_id | bank code: $bank_codee");
+                    }else{
+                        logger("VA COULD NOT BE GENERATED FOR $first_name | $user_id | bank code: $bank_codee");
+
                     }
-                    sleep(2);
+                    sleep(1);
 
                     //it means its been generated already
                 }
