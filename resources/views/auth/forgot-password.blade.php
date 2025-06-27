aa31279a10bdd1d9abb9d0d22d73d58650490828
Sammiee4real <adebsholey4real@gmail.com>
2025-06-27 06:45:07 +0100

    put hot sales on web on hold for now

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -40,6 +40,13 @@
         /></noscript>
         <!-- End Meta Pixel Code -->
   @endif
+
+  @php
+  $admin_site_color =  App\Models\AdminColorSetting::where('color_name','admin_site_color')->first();
+  $admin_site_color_value = $admin_site_color->color_value ?? (int) '90, 102, 241'; 
+ //  echo $admin_site_color_value;  
+ @endphp
+
   
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -66,6 +73,34 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
+
+
+   
+   {{-- <style>
+     :root {
+           --color-primary: {{  $admin_site_color_value  }};
+           /* --color-primary: 90 102 241; */
+           --color-primary-rgb: 90,102,241;
+           --color-secondary: 96 165 250;
+           --color-success: 34 197 94;
+           --color-info: 76 117 207;
+           --color-warning: 234 179 8;
+           --color-danger: 244 63 94;
+           --body-bg: 242 246 249;
+           --default-text-color: 71 85 105;
+           --default-border: 243 243 243;
+           --muted: 140 144 151;
+           --dark-rgb: 14 16 20;
+           --menu-bg: 255 255 255;
+           --menu-border-color: 243 243 243;
+           --menu-prime-color: 100 116 139;
+           --header-bg: 255 255 255;
+           --header-prime-color: 100 116 139;
+           --header-border-color: 243 243 243;
+           --dark-bg: 30 41 59;
+           --dark-bg2: 249 250 251;
+       }
+    </style> --}}
 
 
 </head>
@@ -190,37 +225,7 @@
     {{-- <script src="../assets/libs/preline/preline.js"></script> --}}
     <script src="{{ asset(env('APP_ASSETS_BASE_URL').'libs/preline/preline.js') }}"></script>
 
-    @php
-     $admin_site_color =  App\Models\AdminColorSetting::where('color_name','admin_site_color')->first();
-     $admin_site_color_value = $admin_site_color->color_value ?? (int) '90, 102, 241'; 
-    //  echo $admin_site_color_value;  
-    @endphp
-
-    <style>
-      :root {
-            --color-primary: {{  $admin_site_color_value  }};
-            /* --color-primary: 90 102 241; */
-            --color-primary-rgb: 90,102,241;
-            --color-secondary: 96 165 250;
-            --color-success: 34 197 94;
-            --color-info: 76 117 207;
-            --color-warning: 234 179 8;
-            --color-danger: 244 63 94;
-            --body-bg: 242 246 249;
-            --default-text-color: 71 85 105;
-            --default-border: 243 243 243;
-            --muted: 140 144 151;
-            --dark-rgb: 14 16 20;
-            --menu-bg: 255 255 255;
-            --menu-border-color: 243 243 243;
-            --menu-prime-color: 100 116 139;
-            --header-bg: 255 255 255;
-            --header-prime-color: 100 116 139;
-            --header-border-color: 243 243 243;
-            --dark-bg: 30 41 59;
-            --dark-bg2: 249 250 251;
-        }
-        </style>
+   
 
 </body>
 
