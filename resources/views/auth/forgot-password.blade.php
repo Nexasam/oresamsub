dfcf12f1b580f627afbeef2bf4e71c2c87dc237b
Sammiee4real <adebsholey4real@gmail.com>
2025-06-27 07:20:36 +0100

    put hot sales on web on hold for now

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -173,6 +173,7 @@
                                 </div> --}}
                                 {{-- <x-auth-session-status class="mb-4" :status="session('status') ?? 'nil'" /> --}}
                                 <!-- Form -->
+                                @php dd(old('email')) @endphp
                                 <form method="POST" action="{{ route('password.email') }}">
                                     @csrf
                                     <div>
@@ -180,7 +181,8 @@
                                             <div>
                                                 <label for="email" class="block text-sm mb-2 dark:text-gray-900">{{__('messages.Email Address')}}</label>
                                                 <div class="relative">
-                                                    <x-text-input id="email" name="email" class="block mt-1 w-full" type="email" email="email" :value="old('email')" required autofocus autocomplete="email" />
+                                                    <x-text-input id="email" name="email" class="block mt-1 w-full" type="email" email="email" required autofocus autocomplete="email" />
+                                                    {{-- <x-text-input id="email" name="email" class="block mt-1 w-full" type="email" email="email" :value="old('email')" required autofocus autocomplete="email" /> --}}
                                                     {{-- <x-input-error :messages="$errors->get('email')" class="mt-2" /> --}}
                                                 </div>
                                             </div>
