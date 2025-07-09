78a89455e0f5e752ab873cb4331406d59da82d9d
Sammiee4real <adebsholey4real@gmail.com>
2025-07-09 06:27:48 +0100

    fix issue with tiers... limit funding until transaction is good

--- app/Console/Commands/ComputeReferralCommission.php
+++ app/Console/Commands/ComputeReferralCommission.php
@@ -33,24 +33,24 @@ class ComputeReferralCommission extends Command
        if(env('APP_NAME') == 'OresamSub'){
                 $start_from_this_date = date('2025-07-08');
                 $today = date('Y-m-d');
-                $yesterday = date('Y-m-d', strtotime('-2 day'));
+                $yesterday = date('Y-m-d', strtotime('-1 day'));
                 $twodaysback = date('Y-m-d', strtotime('-2 day'));
 
-                $fetch_yest_successful_txns = Transaction::with(['user.user_plan','product_plan'])
+                $fetch_successful_txns = Transaction::with(['user.user_plan','product_plan'])
                                         ->whereDate('created_at','>=',$twodaysback)
                                         // ->whereDate('created_at','like','%'.$today.'%') #use yesterday after testing
                                         ->whereStatus(1)
                                         ->get();
 
-                if(count($fetch_yest_successful_txns) > 0){
-                    foreach($fetch_yest_successful_txns as $yest_successful_txn){
-                        $txn_id = $yest_successful_txn->id;
-                        $user = $yest_successful_txn->user;
-                        $user_id = $yest_successful_txn->user_id;
-                        $plan_level = $yest_successful_txn->user->user_plan->plan_level;
+                if(count($fetch_successful_txns) > 0){
+                    foreach($fetch_successful_txns as $successful_txn){
+                        $txn_id = $successful_txn->id;
+                        $user = $successful_txn->user;
+                        $user_id = $successful_txn->user_id;
+                        $plan_level = $successful_txn->user->user_plan->plan_level;
                         $commission_field = 'user_level_'.$plan_level.'_commission';
-                        $expected_commission = $yest_successful_txn->product_plan->$commission_field;//continue here
-                        $upline_id = $yest_successful_txn->user->upline_id;
+                        $expected_commission = $successful_txn->product_plan->$commission_field;//continue here
+                        $upline_id = $successful_txn->user->upline_id;
 
                         if($expected_commission != 0 && $upline_id != NULL){
                             //insert
@@ -60,19 +60,19 @@ class ComputeReferralCommission extends Command
                                     //upline exists
                                     if(! Commissions::where('transaction_id',$txn_id)->first() ){
                                         $commissionssss = Commissions::create([
-                                            'transaction_id' => $yest_successful_txn->id,
+                                            'transaction_id' => $successful_txn->id,
                                             'commission' => $expected_commission,
                                             'beneficiary' => $user_upline_check->id,
-                                            'transaction_by' => $yest_successful_txn->user_id,
+                                            'transaction_by' => $successful_txn->user_id,
                                         ]);
                                     }
                                     
 
                                     logger('Commission added for:'.json_encode([
-                                        'transaction_id' => $yest_successful_txn->id,
+                                        'transaction_id' => $successful_txn->id,
                                         'commission' => $expected_commission,
                                         'beneficiary' => $user_upline_check->id,
-                                        'transaction_by' => $yest_successful_txn->user_id,
+                                        'transaction_by' => $successful_txn->user_id,
                                     ]));
                             }else{
                                 logger('upline not found for user or user is found as upline of self: '.$user->username.' with txn id: '.$txn_id);
