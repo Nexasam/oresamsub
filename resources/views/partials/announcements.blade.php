c9c64bcc654ef1d4256c47b9f73f0971579d3238
Sammiee4real <adebsholey4real@gmail.com>
2025-07-23 12:34:57 +0100

    deploy modal change

--- resources/views/partials/announcements.blade.php
+++ resources/views/partials/announcements.blade.php
@@ -34,7 +34,7 @@
 
 
 
-    <div 
+    {{-- <div 
     x-data="{ open: true }" 
     x-show="open"
     x-init="setTimeout(() => open = true, 500)" 
@@ -63,6 +63,41 @@
           <br><hr><br>
         @endforeach
     </div>
+</div> --}}
+
+
+
+
+<div 
+    x-data="{ open: true }" 
+    x-show="open"
+    x-init="setTimeout(() => open = true, 500)" 
+    class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-60"
+    @click.self="open = false" 
+>
+    <div 
+        class="bg-white rounded-xl shadow-lg p-6 w-full max-w-md relative overflow-y-auto max-h-[90vh]"
+        @click.stop 
+    >
+        <button @click="open = false" class="absolute top-2 right-2 text-gray-900 hover:text-red-500 text-xl font-bold">
+            &times;
+        </button>
+
+        <h2 class="text-xl font-bold text-gray-800 mb-2">🎉 {{__('messages.Announcements')}}!</h2>
+
+        <!-- New Close Button -->
+        <button @click="open = false" class="mb-4 px-4 py-2 bg-gray-800 text-white rounded hover:bg-gray-900 transition">
+            Close
+        </button>
+
+        @foreach ($announcements as $ann)
+          <div class="bg-green-100 p-4 mb-4 rounded">
+            {!! $ann->description !!}  
+          </div> 
+          <hr class="border-gray-300 mb-4">
+        @endforeach
+    </div>
 </div>
+
 
 @endif
