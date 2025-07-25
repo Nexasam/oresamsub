4a0ec85b02d3caf666eabd74298aa2a6c3e58d31
Sammiee4real <adebsholey4real@gmail.com>
2025-07-25 08:38:56 +0100

    deploy modal change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -919,11 +919,11 @@
                                   if(product_slug == 'data'){
 
                                     
-                                    if(env('APP_NAME') == 'OresamSub'){
+                                    // if(env('APP_NAME') == 'OresamSub'){
                                       option = "<option value="+idd+">"+product_plan_name+"- &#8358; "+selling_price+" - Upline Commission:&#8358;"+upline_commission+"</option>";
-                                    }else{
-                                      option = "<option value="+idd+">"+product_plan_name+'- &#8358;'+selling_price+"</option>";
-                                    }
+                                    // }else{
+                                    //   option = "<option value="+idd+">"+product_plan_name+'- &#8358;'+selling_price+"</option>";
+                                    // }
                                   
                                   
                                   }
@@ -936,11 +936,11 @@
                                   else if(product_slug == 'airtime' && amount == ''){
                                     option = "<option value="+idd+">"+product_plan_name+"</option>";
                                   }else{
-                                    if(env('APP_NAME') == 'OresamSub'){
+                                    // if(env('APP_NAME') == 'OresamSub'){
                                       option = "<option value="+idd+">"+product_plan_name+" &nbsp;&nbsp;Upline Commission:&#8358;"+upline_commission+"</option>";
-                                    }else{
-                                      option = "<option value="+idd+">"+product_plan_name+"</option>";
-                                    }
+                                    // }else{
+                                    //   option = "<option value="+idd+">"+product_plan_name+"</option>";
+                                    // }
                                   }
                                   $('#product_plan_id').append(option);
                                 
