e87c923a1704fbd96d72a1dc1bb8c40c1a226797
Sammiee4real <adebsholey4real@gmail.com>
2025-08-15 16:33:50 +0100

    fix issues

--- resources/views/transaction_details.blade.php
+++ resources/views/transaction_details.blade.php
@@ -28,7 +28,7 @@
                     {{-- <a class="underline font-extrabold text-blue-700" target="_blank" href="{{ route('admin.product_plans.product_plan_details',$data->product_plan->id) }}">Go to Plan Details: {{ $data->product_plan->product_plan_name }}</a> <br><br> --}}
                     <a class="underline font-extrabold text-blue-700" href="{{ route('admin.product_plan_categories.view_details_by_automation',['id' => $data->product_plan->product_plan_category->id, 'automation_id' =>$data->product_plan->automation->id]) }}">See the Automation: {{  $data->product_plan->automation->automation_name }}</a> <br><br>
 
-                    <div class="bg-white dark:bg-gray-800 p-6 rounded-xl shadow-md space-y-3">
+                    <div class="bg-white dark:bg-gray-800 p-6 rounded-xl shadow-md space-y-3 mb-2">
                       @php
                           $automationss = App\Models\Automation::select('domain_url','automation_name')->get();
                       @endphp
