5a777443175fecc7707ae1fc28179f3b5ae1308e
Sammiee4real <adebsholey4real@gmail.com>
2025-08-15 20:00:50 +0100

    fix issues

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -593,7 +593,7 @@ class TransactionController extends Controller
         $transaction_details->update([
             'status' => 1,
             // 'user_screen_message' => $request->success_message,
-            'admin_screen_message' => 'MANUALLY RESOLVED: automation:'.$automation_name.' by '.auth()->user()->email. auth()->user()->first_name.'  message:'.$request->success_message,
+            'admin_screen_message' => 'MANUAL: automation:'.$automation_name.' by '.auth()->user()->email. auth()->user()->first_name.'  message:'.$request->success_message,
             'set_for_manual' => 0,
             'manually_processed_by' => $userinfooo,
         ]); 
