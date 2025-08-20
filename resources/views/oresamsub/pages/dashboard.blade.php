3839323895e46c2d951737f853442293143678de
Sammiee4real <adebsholey4real@gmail.com>
2025-08-20 09:26:39 +0100

    funding automation accts

--- resources/views/oresamsub/pages/dashboard.blade.php
+++ resources/views/oresamsub/pages/dashboard.blade.php
@@ -128,7 +128,7 @@
     <!-- Text -->
     <div>
       <h2 class="text-lg md:text-lg font-bold">🔥 Join Our Community</h2>
-      <p class="text-sm text-white/90 mt-1">
+      <p class="text-xs text-white/90 mt-1">
         Get <span class="font-semibold">real-time updates</span>, promos & special alerts directly in our WhatsApp community.  
       </p>
     </div>
