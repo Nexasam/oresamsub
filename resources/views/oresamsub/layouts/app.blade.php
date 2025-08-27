9b639942a08bff703050e5d48a5dc25bab32551b
Sammiee4real <adebsholey4real@gmail.com>
2025-08-27 05:26:55 +0100

    oresamsub deploy test

--- resources/views/oresamsub/layouts/app.blade.php
+++ resources/views/oresamsub/layouts/app.blade.php
@@ -5,6 +5,9 @@
   <title>{{ $title ?? 'OresamSub' }}</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+  <link rel="icon" type="image/png" href="{{ asset('assets/logo_imgs/favicon/android-chrome-192x192.png') }}">
+
+
   <!-- DARK MODE PREVENT FLASH -->
   <script>
     if (localStorage.getItem('theme') === 'dark' ||
