4ddb6eea5f27aa7250dcfac9182cb70e3bde5caf
Sammiee4real <adebsholey4real@gmail.com>
2025-09-01 18:27:58 +0100

    plans rearrangement to address pricing

--- resources/views/admin/unique_product_plans/index.blade.php
+++ resources/views/admin/unique_product_plans/index.blade.php
@@ -154,7 +154,19 @@
 
                             <div class="flex justify-end space-x-2 mt-6">
                                 <button type="button" @click="$store.pricingModal.closeModal()" class="px-4 py-2 bg-gray-300 rounded">Cancel</button>
-                                <button type="submit" class="px-4 py-2 bg-blue-600 text-white rounded">Save</button>
+                                <button type="submit"
+                                    class="px-4 py-2 bg-blue-600 text-white rounded flex items-center justify-center"
+                                    :disabled="$store.pricingModal.loading">
+                                <span x-show="!$store.pricingModal.loading">Save</span>
+                                <span x-show="$store.pricingModal.loading" class="flex items-center space-x-2">
+                                    <svg class="animate-spin h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                                        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8H4z"></path>
+                                    </svg>
+                                    <span>Saving...</span>
+                                </span>
+                            </button>
+
                             </div>
                         </form>
                     </div>
