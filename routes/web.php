6f878a8e93e11492a14795a21dedf056ae044627
Sammiee4real <adebsholey4real@gmail.com>
2025-09-01 13:06:49 +0100

    plans rearrangement to address pricing

--- routes/web.php
+++ routes/web.php
@@ -566,8 +566,8 @@ Route::middleware(['set_locale'])->group(function () {
 
 
 
-            Route::middleware(['auth','verified','admin'])->get('admin/unique_product_plans', [UniqueProductPlansController::class, 'index'])->name('admin.unique_product_plans.index');
-            Route::middleware(['auth','verified','admin'])->get('admin/walletlogs/admin_fetch_wallet_logs', [UniqueProductPlansController::class, 'admin_fetch_unique_product_plans'])->name('admin.unique_product_plans.admin_fetch_wallet_logs');
+            Route::middleware(['auth','verified','admin'])->get('admin/unique_product_plans/index', [UniqueProductPlansController::class, 'index'])->name('admin.unique_product_plans.index');
+            Route::middleware(['auth','verified','admin'])->get('admin/unique_product_plans/admin_fetch_unique_product_plans', [UniqueProductPlansController::class, 'admin_fetch_unique_product_plans'])->name('admin.unique_product_plans.admin_fetch_wallet_logs');
 
 
             Route::middleware(['auth','verified','admin'])->get('admin/product_plans', [ProductPlanController::class, 'index'])->name('admin.product_plans.index');
