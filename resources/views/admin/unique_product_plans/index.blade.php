a981c3a36c6fd047775b558dc65a93033e45d133
Sammiee4real <adebsholey4real@gmail.com>
2025-09-01 12:20:18 +0100

    plans rearrangement to address pricing

--- resources/views/admin/unique_product_plans/index.blade.php
+++ resources/views/admin/unique_product_plans/index.blade.php
@@ -149,6 +149,7 @@
                                               </template>
                                           </tbody>
                                           
+                                          
 
 
                                         </table>
