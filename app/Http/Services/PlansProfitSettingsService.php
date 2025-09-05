0c570c3362630baa7f6f9a620df83f2539abe594
Sammiee4real <adebsholey4real@gmail.com>
2025-09-05 23:03:15 +0100

    get selling price

--- app/Http/Services/PlansProfitSettingsService.php
+++ app/Http/Services/PlansProfitSettingsService.php
@@ -15,15 +15,15 @@ use App\Models\FundingOptionBankCodes;
 class PlansProfitSettingsService{
 
     public function getSellingPriceForCustomer(){
-        $plans = ProductPlan::all();
+        $plans = ProductPlan::with('product','automation')->get();
         $dataa = [];
 
         foreach($plans as $key=>$plan){
 
             $profit_setting = PlanProfitSetting::where('data_size_in_mb',$plan->data_size_in_mb)
-            ->where('product_id',$plan->product_id)
-            ->where('validity_in_days',$plan->validity)
-            ->where('data_size_in_mb',$plan->size)
+            ->where('product_id',$plan->product->id)
+            ->where('validity_in_days',$plan->validity_in_days)
+            ->where('data_size_in_mb',$plan->data_size_in_mb)
             ->where('is_social',$plan->is_social)
             ->first(); 
 
@@ -32,7 +32,7 @@ class PlansProfitSettingsService{
             $profit_level = "profit_".$userplan_level;
             $dataa[$key]['profit_level'] = $userplan_level;
             $dataa[$key]['profit'] = abs($profit_setting->$profit_level);
-            $dataa[$key]['selling_price'] = $profit_setting->$profit_level + $plan->cost_price;
+            $dataa[$key]['selling_price'] = $profit_setting->$profit_level + 50;
         }
 
         return [
