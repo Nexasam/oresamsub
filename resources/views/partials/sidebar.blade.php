472ac2fff87af70f071239cbd77dc6f15e3ba2cf
Sammiee4real <adebsholey4real@gmail.com>
2025-09-08 13:30:23 +0100

    add validation bvn

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -325,6 +325,19 @@
                         </li>
                         <!-- End::slide -->
 
+                         <!-- Start::slide -->
+                         <li class="slide  has-sub">
+                            <a href="{{ route('admin.plan_profit_settings.index')}}" class="side-menu__item">
+                                <i class="ti ti-cash side-menu__icon"></i>
+                                <span class="side-menu__label">Profit Settings</span>
+                                <i class="ri ri-arrow-right-s-line side-menu__angle"></i>
+                            </a>
+                            <ul class="slide-menu child1">
+                            </ul>
+                        </li>
+                        <!-- End::slide -->
+
+
                   @endif
 
                   <!-- Start::slide -->
