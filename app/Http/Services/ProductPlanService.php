bd4133fd8193c5e2ed20d57118e72dde317727bb
Sammiee4real <adebsholey4real@gmail.com>
2025-10-13 18:26:31 +0100

    create plan

--- app/Http/Services/ProductPlanService.php
+++ app/Http/Services/ProductPlanService.php
@@ -22,53 +22,53 @@ class ProductPlanService{
 
         // if(count($product_plans) >= 1){
 
-            foreach($data as $key=>$product_plan){
-                $cost_price = $product_plan['cost_price'];
-                $data_size_in_mb = $product_plan['data_size_in_mb'];
-             
-                $network_id = $product_plan->product_plan_category->network->id;
-                $product_id = $product_plan->product_plan_category->product->id;
-                $slug = Product::select('slug')->where('id',$product_id)->first()->slug;
+        foreach($data as $key=>$product_plan){
+            $cost_price = $product_plan->cost_price;
+            $data_size_in_mb = $product_plan->data_size_in_mb;
+            
+            $network_id = $product_plan->product_plan_category->network->id;
+            $product_id = $product_plan->product_plan_category->product->id;
+            $slug = Product::select('slug')->where('id',$product_id)->first()->slug;
 
-                $product_planss[$key]['plan_id'] =  (int)$product_plan->api_id ?? NULL; //api for api calls
-                $product_planss[$key]['product_plan_id'] = $product_plan->id;
-        
-                for($i=1; $i <=12; $i++){
+            $product_planss[$key]['plan_id'] =  (int)$product_plan->api_id ?? NULL; //api for api calls
+            $product_planss[$key]['product_plan_id'] = $product_plan->id;
+    
+            for($i=1; $i <=12; $i++){
 
-                    $user_level = "user_level_{$i}_selling_price";
-                    if($slug == 'data' || $slug == 'cable_subscription'){
-                        if($slug == 'data'){
-                            $get_planprofit = PlanProfitSetting::where('network_id',$network_id)
-                            ->where('product_id',$product_id)
-                            ->where('data_size_in_mb',$data_size_in_mb)
-                            ->first();
-                    
-                            $profitlevel_for_user = "profit_$i" ?? 'profit_1';
-                            $profit_value = $get_planprofit->$profitlevel_for_user ?? 50;
-                            $selling_price = $cost_price + abs(num: $profit_value);
-
-                        }else{
-                            $selling_price = $product_plan->$user_level ?? 5000;
-                        }
+                $user_level = "user_level_{$i}_selling_price";
+                if($slug == 'data' || $slug == 'cable_subscription'){
+                    if($slug == 'data'){
+                        $get_planprofit = PlanProfitSetting::where('network_id',$network_id)
+                        ->where('product_id',$product_id)
+                        ->where('data_size_in_mb',$data_size_in_mb)
+                        ->first();
+                
+                        $profitlevel_for_user = "profit_$i" ?? 'profit_1';
+                        $profit_value = $get_planprofit->$profitlevel_for_user ?? 50;
+                        $selling_price = $cost_price + abs(num: $profit_value);
 
                     }else{
-                        //airtime and electricity for now
-                        $selling_profit = $product_plan->$user_level ?? 1; //percent
-                        $selling_price = $selling_profit; //percent
+                        $selling_price = $product_plan->$user_level ?? 5000;
                     }
 
-                    $product_planss[$key]['cost_price_aff_'.$i] = $selling_price;
-
+                }else{
+                    //airtime and electricity for now
+                    $selling_profit = $product_plan->$user_level ?? 1; //percent
+                    $selling_price = $selling_profit; //percent
                 }
-               
-                $product_planss[$key]['is_api'] = 'yes';  
-                $product_planss[$key]['product_id'] = $product_id;  
-                $product_planss[$key]['network_id'] = $network_id;  
-                $product_planss[$key]['product_plan_name'] = $product_plan->product_plan_name;
-                $product_planss[$key]['data_size_in_mb'] = $product_plan->data_size_in_mb;
-                $product_planss[$key]['validity_in_days'] = $product_plan->validity_in_days; 
-                             
+
+                $product_planss[$key]['cost_price_aff_'.$i] = $selling_price;
+
             }
+            
+            $product_planss[$key]['is_api'] = 'yes';  
+            $product_planss[$key]['product_id'] = $product_id;  
+            $product_planss[$key]['network_id'] = $network_id;  
+            $product_planss[$key]['product_plan_name'] = $product_plan->product_plan_name;
+            $product_planss[$key]['data_size_in_mb'] = $product_plan->data_size_in_mb;
+            $product_planss[$key]['validity_in_days'] = $product_plan->validity_in_days; 
+                            
+        }
 
         // }else{
         //     $product_planss[0]['cost_price'] = NULL;
