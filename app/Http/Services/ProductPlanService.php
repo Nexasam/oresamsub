d503899ed67d5d5bdd1c511fbd030244d776baf3
Sammiee4real <adebsholey4real@gmail.com>
2025-10-29 13:12:18 +0100

    check new admin flow

--- app/Http/Services/ProductPlanService.php
+++ app/Http/Services/ProductPlanService.php
@@ -151,6 +151,8 @@ class ProductPlanService{
                 $product_planss[$key]['product_plan_name'] = $product_plan->product_plan_name;
                 $product_planss[$key]['data_size_in_mb'] = (int) $product_plan->data_size_in_mb;
                 $product_planss[$key]['validity_in_days'] = (int) $product_plan->validity_in_days;
+                $product_planss[$key]['visibility'] = (int) $product_plan->visibility;
+                $product_planss[$key]['public_visibility'] = (int) $product_plan->public_visibility;
             }
 
             // Always return
