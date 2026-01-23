c4963b3780c40adecb29d192246648e5369278fb
Sammiee4real <adebsholey4real@gmail.com>
2026-01-23 09:17:47 +0100

    add comms to users page

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -11,7 +11,7 @@ import Announcements from "@/Components/Announcements";
 
 export default function Dashboard({ transactions: initialTransactions }) {
   const { props } = usePage();
-  const { auth, announcements, impersonator } = props;
+  const { auth, announcements, impersonator, commissionData } = props;
   const user = auth.user;
 
   const transactions = initialTransactions ?? props.transactions ?? [];
