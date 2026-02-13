b4ea6fe83e806e9a3dd90182c49ac9f258a4a5e5
Sammiee4real <adebsholey4real@gmail.com>
2026-02-13 12:52:13 +0100

    fix onboarding

--- app/Http/Controllers/InertiaDashboardController.php
+++ app/Http/Controllers/InertiaDashboardController.php
@@ -102,7 +102,7 @@ class InertiaDashboardController extends Controller
                 'user_virtual_accounts.account_name',
                 'user_virtual_accounts.account_number',
                 'user_virtual_accounts.bank_code',
-                'funding_option_bank_codes.description as bank_description'
+                'funding_option_bank_codes.short_description as bank_description'
             )
             ->leftJoin('funding_option_bank_codes', function($join) {
                 $join->on('user_virtual_accounts.funding_option_id', '=', 'funding_option_bank_codes.funding_option_id')
