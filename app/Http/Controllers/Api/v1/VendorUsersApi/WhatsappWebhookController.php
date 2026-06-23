bc879719dfddb5d0cf15706e5724ab6b6a391b26
Sammiee4real <adebsholey4real@gmail.com>
2026-06-23 21:55:13 +0100

    fix iss

--- app/Http/Controllers/Api/v1/VendorUsersApi/WhatsappWebhookController.php
+++ app/Http/Controllers/Api/v1/VendorUsersApi/WhatsappWebhookController.php
@@ -1,9 +1,10 @@
 <?php
 namespace App\Http\Controllers\Api\v1\VendorUsersApi;
 
-use Illuminate\Http\Request;
-use App\Traits\JsonResponseWrapper;
 use App\Http\Controllers\Controller;
+use App\Traits\JsonResponseWrapper;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 // use App\Http\Services\Api\v1\VendorUsersApi\Products\ProductsService;
 // use App\Services\Api\Automation\MegaSubPlugAutomation\MegaSubCableTV;
 
@@ -13,9 +14,13 @@ class WhatsappWebhookController extends Controller
     use JsonResponseWrapper;
 
     public function receive(Request $request){
-        \Log::info('WHATSAPP WEBHOOK', [
-            'payload' => $request->all()
-        ]);
+        Log::info(
+            'WHATSAPP WEBHOOK',
+            json_decode(
+                json_encode($request->all()),
+                true
+            )
+        );
     
         return response()->json([
             'success' => true
