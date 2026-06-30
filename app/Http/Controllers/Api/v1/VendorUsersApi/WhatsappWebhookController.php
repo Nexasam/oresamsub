8b85b8761e60602c07c1e0320d9645a865be7211
Sammiee4real <adebsholey4real@gmail.com>
2026-06-30 17:49:44 +0100

    fix

--- app/Http/Controllers/Api/v1/VendorUsersApi/WhatsappWebhookController.php
+++ app/Http/Controllers/Api/v1/VendorUsersApi/WhatsappWebhookController.php
@@ -409,60 +409,37 @@ class WhatsappWebhookController extends Controller
         /*
         Send reply
         */
-        app(Whatsappsender::class)->send(
-            $phone,
-            $result['message']
-        );
+        if (
+            in_array(
+                $result['status'] ?? '',
+                [
+                    'data_awaiting_confirmation',
+                    'airtime_awaiting_confirmation',
+                ]
+            )
+        ) {
+
+            app(Whatsappsender::class)
+                ->sendConfirmationButtons(
+                    $phone,
+                    $result['message']
+                );
+
+        } else {
+
+            app(Whatsappsender::class)
+                ->send(
+                    $phone,
+                    $result['message']
+                );
+        }
 
         return response()->json([
             'ok' => true
         ]);
     }
 
-    public function receiveold(Request $request, Whatsappsender $sender, IntentRouter $router)
-    {
-        $message = $request->input('entry.0.changes.0.value.messages.0');
-
-        if (!$message) {
-            // logger('omo..na this one oh');
-            return response()->json(['success' => true]);
-        }
-
-
-   
-
-        $phone = $message['from'] ?? null;
-        $text = trim($message['text']['body'] ?? '');
-
-        
-
-        $intentData = $router->resolve($text);
-
-        $response = match ($intentData['intent']) {
-
-            'account' => app(\App\Services\Whatsapp\AccountHandler::class)->handle($phone),
-
-            'services' => app(\App\Services\Whatsapp\ServicesHandler::class)->handle($intentData['raw']),
-
-            'favorites' => app(\App\Services\Whatsapp\FavouritesHandler::class)->handle($phone),
-
-            'offers' => app(\App\Services\Whatsapp\OffersHandler::class)->handle($phone),
-
-            'about' => app(\App\Services\Whatsapp\AboutHandler::class)->handle(),
-
-            default => "🤖 I didn't understand that.\nTry: DATA, ACCOUNT, OFFERS"
-        };
-
-        Log::info('WhatsApp Outgoing Response', [
-            'phone' => $phone,
-            'response' => $response,
-            'intent' => $intentData['intent'] ?? null,
-        ]);
-
-        $sender->send($phone, $response);
-
-        return response()->json(['success' => true]);
-    }
+ 
 
   
    
