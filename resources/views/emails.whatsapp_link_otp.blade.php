aa9a115c100804cb71b833d2f52e2534cd01c584
Sammiee4real <adebsholey4real@gmail.com>
2026-07-01 13:55:46 +0100

    whatsapp

---
+++ resources/views/emails.whatsapp_link_otp.blade.php
@@ -0,0 +1,11 @@
+<h2>WhatsApp Verification</h2>
+
+<p>Hello,</p>
+
+<p>Your OresamSub verification code is:</p>
+
+<h1>{{ $otp }}</h1>
+
+<p>This code expires in 10 minutes.</p>
+
+<p>If you did not request this verification, please ignore this email.</p>
