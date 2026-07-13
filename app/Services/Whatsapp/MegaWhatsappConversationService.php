d1fbb70f702496deaa652f6e875d5e1261d30768
Sammiee4real <adebsholey4real@gmail.com>
2026-07-13 09:41:34 +0100

    fix iss

--- app/Services/Whatsapp/MegaWhatsappConversationService.php
+++ app/Services/Whatsapp/MegaWhatsappConversationService.php
@@ -446,25 +446,52 @@ class MegaWhatsappConversationService
         //     ->orderBy('user_level_1_selling_price')
         //     ->get();
 
+        // $plans = ProductPlan::query()
+        // ->where(
+        //     'product_plan_category_id',
+        //     $category->id
+        // )
+        // ->where('visibility', 1)
+        // ->orderByRaw("
+        //     CASE
+        //         WHEN data_size_in_mb = 500 THEN 1
+        //         WHEN data_size_in_mb = 1000 THEN 2
+        //         WHEN data_size_in_mb = 2000 THEN 3
+        //         WHEN data_size_in_mb = 3000 THEN 4
+        //         WHEN data_size_in_mb = 5000 THEN 5
+        //         ELSE 6
+        //     END
+        // ")
+        // ->orderByRaw('CAST(data_size_in_mb AS UNSIGNED) DESC')
+        // ->take(20)
+        // ->get();
+
         $plans = ProductPlan::query()
         ->where(
             'product_plan_category_id',
             $category->id
         )
         ->where('visibility', 1)
-        ->orderByRaw("
-            CASE
-                WHEN data_size_in_mb = 500 THEN 1
-                WHEN data_size_in_mb = 1000 THEN 2
-                WHEN data_size_in_mb = 2000 THEN 3
-                WHEN data_size_in_mb = 3000 THEN 4
-                WHEN data_size_in_mb = 5000 THEN 5
-                ELSE 6
-            END
-        ")
-        ->orderByRaw('CAST(data_size_in_mb AS UNSIGNED) DESC')
-        ->take(20)
-        ->get();
+        ->get()
+        ->sortBy(function ($plan) {
+
+            $priority = [
+                500 => 1,
+                1000 => 2,
+                2000 => 3,
+                3000 => 4,
+                5000 => 5,
+            ];
+
+            $size = (int) $plan->data_size_in_mb;
+
+            return [
+                $priority[$size] ?? 999,
+                -$size,
+            ];
+        })
+        ->take(10)
+        ->values();
     
         if ($plans->isEmpty()) {
     
